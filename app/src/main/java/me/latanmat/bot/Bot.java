f8449516ac50c3a2663f105a8bcb33fe377844dd
latanam <latanam@wwu.edu>
2022-03-18 10:44:01 -0700

    Minor updates and added the MySQL jar

--- app/src/main/java/me/latanmat/bot/Bot.java
+++ app/src/main/java/me/latanmat/bot/Bot.java
@@ -5,12 +5,16 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
+import javax.security.auth.login.LoginException;
+
 public class Bot {
 
-    public static void main(String[] args){
-        JDABuilder builder = JDABuilder.createDefault("OTU0MTQ4NTYwNDgyNDkyNTA2.YjO6GA.TtC1YtDL4kx8N65Vf5MNVCt6OCk");
+    public static void main(String[] args) throws LoginException {
+        //will make a config file in the future
+        JDABuilder builder = JDABuilder.createDefault("OTU0MTQ4NTYwNDgyNDkyNTA2.YjO6GA.ZDjHv5MwYfABtAuARA4nfpXyTcs");
         builder.setActivity(Activity.playing("Making a Discord Bot"));
         builder.setStatus(OnlineStatus.ONLINE);
         builder.addEventListeners(new Listener());
+        builder.build();
     }
 }
