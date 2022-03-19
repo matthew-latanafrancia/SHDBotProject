45c776f45d321ad1a297e0ee784bb7d37f87875a
latanam <latanam@wwu.edu>
2022-03-19 15:47:18 -0700

    Implemented flipcoin and userinfo commands in separate classes.

---
+++ app/src/main/java/me/latanmat/bot/Commands/Flipcoin.java
@@ -0,0 +1,35 @@
+package me.latanmat.bot.Commands;
+
+import java.util.Random;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class Flipcoin {
+    public static boolean flipcoinCommand(GuildMessageReceivedEvent event){
+        /*
+        The flipcoinCommand function returns true or false
+        Output:
+        true: when the user wins the coin flip
+        false: when the user loses the coin flip
+         */
+        Random random = new Random();
+        int randomNumber = random.nextInt(2);
+
+        if(randomNumber == 1){
+            event.getMessage().reply("Congratulations!  You won!").queue();
+            return true;
+        }
+        else{
+            event.getMessage().reply("You lost. Try your luck next time!").queue();
+            return false;
+        }
+
+    }
+
+    public static String helpToString(){
+
+        String retString = "**!flipcoin [# of coins]**: Bet against the bot and win some coins!\n";
+
+        return retString;
+    }
+}
---
+++ app/src/main/java/me/latanmat/bot/Commands/UserInfo.java
@@ -0,0 +1,31 @@
+package me.latanmat.bot.Commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.awt.*;
+
+public class UserInfo {
+    public static void userInfoCommand(GuildMessageReceivedEvent event, Litcoin currUser){
+        User usertoLookAt = event.getAuthor();
+        String userID = usertoLookAt.getId();
+        String userAvatar = usertoLookAt.getAvatarUrl();
+        String userName = usertoLookAt.getName();
+
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle(userName);
+        embed.setImage(userAvatar);
+        embed.addField("Discord ID:", userID, false);
+        embed.addField("Number of coins:", Integer.toString(currUser.getNumberOfCoins()), false);
+        embed.setColor(Color.CYAN);
+        embed.setFooter("Bot created by Dez", event.getGuild().getOwner().getUser().getAvatarUrl());
+
+        event.getChannel().sendMessageEmbeds(embed.build()).queue();
+    }
+
+    public static String helpToString(){
+        String retString = "**!userinfo**: time to look at some of that information.\n";
+        return retString;
+    }
+}
