b4c74a8c4812023cea2bfc861b9b3aaf66b80457
latanam <latanam@wwu.edu>
2022-03-22 01:55:16 -0700

    Minor changes to the ChallengeThread class

--- app/src/main/java/me/latanmat/bot/Commands/ChallengeThread.java
+++ app/src/main/java/me/latanmat/bot/Commands/ChallengeThread.java
@@ -1,20 +1,16 @@
 package me.latanmat.bot.Commands;
 
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
-import java.sql.SQLException;
 import java.util.List;
-
-import static org.graalvm.compiler.options.OptionType.User;
 
 public class ChallengeThread implements Runnable{
 
     private GuildMessageReceivedEvent event;
     private Litcoin idOfOpponent;
     private Litcoin  idOfUser;
-    List<Member> mentioned;
+
     public ChallengeThread(GuildMessageReceivedEvent event, Litcoin idOfOpponent, Litcoin idOfUser){
             this.event = event;
             this.idOfOpponent = idOfOpponent;
